9b8c57c52b9bbde68a83c4eb0e759d0127312e01
Shad Sharma <shadanan@gmail.com>
2020-04-26 00:47:08 -0700

    Add support for multiple XAirs.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sanic import Sanic
 
+from sanic.exceptions import NotFound
 from sanic.response import json
 from sanic.websocket import WebSocketProtocol
 from json import dumps
@@ -8,8 +9,7 @@ import pyxair
 
 
 app = Sanic(name="XAir API Proxy")
-xinfo = pyxair.auto_detect()
-xairs = {xinfo.name: pyxair.XAir(xinfo)}
+xairs = pyxair.XAirTaskManager()
 
 
 CORS_HEADERS = {
@@ -18,31 +18,46 @@ CORS_HEADERS = {
 }
 
 
-@app.get("/xair/<xair:string>/osc/<address:path>")
-async def osc_get(req, xair, address):
+def get_xair(name):
+    xinfos = {xinfo for xinfo in xairs.list_xairs() if xinfo.name == name}
+    if len(xinfos) == 0:
+        raise NotFound(f"Requested XAir {name} not found")
+    return xairs.get_xair(xinfos.pop())
+
+
+@app.get("/xair")
+async def xair(req):
+    return json({"xair": [x.name for x in xairs.list_xairs()]}, headers=CORS_HEADERS)
+
+
+@app.get("/xair/<name:string>/osc/<address:path>")
+async def osc_get(req, name, address):
     address = "/" + address
-    message = await xairs[xair].get(address)
-    return json({**message._asdict(), **{"xair": xair}}, headers=CORS_HEADERS)
+    xair = get_xair(name)
+    message = await xair.get(address)
+    return json({**message._asdict(), **{"xair": name}}, headers=CORS_HEADERS)
 
 
-@app.patch("/xair/<xair:string>/osc/<address:path>")
-async def osc_patch(req, xair, address):
-    xairs[xair].put(req.json["address"], req.json["arguments"])
-    return json({**req.json, **{"xair": xair}}, headers=CORS_HEADERS)
+@app.patch("/xair/<name:string>/osc/<address:path>")
+async def osc_patch(req, name, address):
+    xair = get_xair(name)
+    xair.put(req.json["address"], req.json["arguments"])
+    return json({**req.json, **{"xair": name}}, headers=CORS_HEADERS)
 
 
-@app.options("/xair/<xair:string>/osc/<address:path>")
-async def osc_options(req, xair, address):
+@app.options("/xair/<name:string>/osc/<address:path>")
+async def osc_options(req, name, address):
     address = "/" + address
-    return json({"xair": xair, "address": address}, headers=CORS_HEADERS)
+    return json({"xair": name, "address": address}, headers=CORS_HEADERS)
 
 
-@app.websocket("/xair/<xair:string>/feed")
-async def feed(req, ws, xair):
-    with xairs[xair].subscribe() as queue:
+@app.websocket("/xair/<name:string>/feed")
+async def feed(req, ws, name):
+    xair = get_xair(name)
+    with xair.subscribe() as queue:
         while True:
             message = await queue.get()
-            await ws.send(dumps({**message._asdict(), **{"xair": xair}}))
+            await ws.send(dumps({**message._asdict(), **{"xair": name}}))
 
 
 if __name__ == "__main__":
@@ -50,6 +65,11 @@ if __name__ == "__main__":
         format="[%(asctime)s] [%(levelname)s] [%(name)s] %(message)s",
         level=logging.INFO,
     )
-    for xair in xairs.values():
-        app.add_task(xair.monitor())
-    app.run(host="0.0.0.0", port=8000, protocol=WebSocketProtocol, auto_reload=True)
+    app.add_task(xairs.start())
+    app.run(
+        host="0.0.0.0",
+        port=8000,
+        protocol=WebSocketProtocol,
+        debug=True,
+        auto_reload=True,
+    )
